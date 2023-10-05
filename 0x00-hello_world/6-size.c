8ef596904b8fbf4f316f6c00fe8e9c277ef8a817
sambajr <metasamba.ng007@gmail.com>
2023-10-05 16:49:22 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,12 @@
 /* Include the standard input/output library*/
 /* Print the size of char*/
 /* Print the size of int*/
-int main(void)
+int main(void)/*main function call*/
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int : %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(float));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
 	return (0);
 }
