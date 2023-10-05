4aefeb8986a3711080e92c6f66414c522f3a0ad4
sambajr <metasamba.ng007@gmail.com>
2023-10-05 15:44:41 -0700

    hurrys

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,9 +7,7 @@
 int main(void)
 {
 	/* Use the printf function to print the message */
-    /* The printf function takes a string literal as its first argument */
-    /* The string literal should be enclosed in double quotes */
-    /* The printf function also requires a semicolon at the end of the statement */
-	printf("with proper grammar, but the outcome is a piece of art,");
+    
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
