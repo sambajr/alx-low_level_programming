6ad1754ddd263fdfc83f2a543ef92070371461ae
sambajr <metasamba.ng007@gmail.com>
2023-10-05 13:53:55 -0700

    h

--- 0x00-hello_world/1-main.c
+++ 0x00-hello_world/1-main.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
-int main(void){
-	printf("Hello julien");
-return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Holberton School");
+	return (0);
 }
