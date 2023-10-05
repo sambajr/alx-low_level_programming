7a24e142e3007cf866b7e0b6cd0f498587e482bf
sambajr <metasamba.ng007@gmail.com>
2023-10-05 15:40:10 -0700

    hurry

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Use the printf function to print the message */
+    /* The printf function takes a string literal as its first argument */
+    /* The string literal should be enclosed in double quotes */
+    /* The printf function also requires a semicolon at the end of the statement */
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
