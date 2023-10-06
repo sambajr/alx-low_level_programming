5b7ac0ce2ca2a5faa4d583d0a7b960900add2371
sambajr <metasamba.ng007@gmail.com>
2023-10-06 11:17:26 -0700

    git push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,9 @@
 /* This is the main function of our program */
 int main(void)
 {
-    /* We use the write function to print a message to the standard error (file descriptor 2) */
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    
-    /* The main function returns 1 as requested */
-    return (1);
+	/*we use the write function to print a message to the standard error*/
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	/*The main function returns 1 as requested*/
+	   return (1);
 }
 
