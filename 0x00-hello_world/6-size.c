c8350ab1726765ca0391876805e543089e8e2aa2
sambajr <metasamba.ng007@gmail.com>
2023-10-06 03:27:17 -0700

    hello size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /* Include the standard input/output library*/
 /* Print the size of char*/
-int main(void)
+int main(void)/* Print the size of char*/
 /*main function call*/
 {
-	fprintf(stderr, "Size of a char: %zu byte(s)\n", sizeof(char));
-	fprintf(stderr, "Size of an int : %zu byte(s)\n", sizeof(int));
-	fprintf(stderr, "Size of a long int: %zu byte(s)\n", sizeof(long));
-	fprintf(stderr, "Size of a long long int: %zu byte(s)\n", sizeof(double));
-	fprintf(stderr, "Size of a float: %zu byte(s)", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int : %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(double));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
 /* Print the size of long long int*/
 	return (0);
 /* Return 0 to indicate successful execution End of the main functio*/
 }
-/* main function returns standard error to the output and prints the sizee of values */
+/*main function returns standard error to the output*/
