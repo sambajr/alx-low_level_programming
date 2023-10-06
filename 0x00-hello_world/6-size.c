aeac74458d7240a29cfb20d0e72f14017edddd4a
sambajr <metasamba.ng007@gmail.com>
2023-10-06 14:56:37 -0700

    ait

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {/*main function call*/
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int : %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(double));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(double));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
 	fprintf(stderr, "[Anything]");
 
 	return (0);
