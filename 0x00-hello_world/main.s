16f5daa52cf090d2e2a87b2c9100e6bfc0a93b46
sambajr <metasamba.ng007@gmail.com>
2023-10-06 10:32:11 -0700

    git

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,10 +1,9 @@
-	.file	"0-main.c"
+	.file	"main.c"
 	.intel_syntax noprefix
 	.text
 	.section	.rodata
-	.align 8
 .LC0:
-	.string	"with proper grammar, but the outcome is a piece of art,"
+	.string	"Holberton School"
 	.text
 	.globl	main
 	.type	main, @function
@@ -18,7 +17,8 @@ main:
 	mov	rbp, rsp
 	.cfi_def_cfa_register 6
 	lea	rdi, .LC0[rip]
-	call	puts@PLT
+	mov	eax, 0
+	call	printf@PLT
 	mov	eax, 0
 	pop	rbp
 	.cfi_def_cfa 7, 8
