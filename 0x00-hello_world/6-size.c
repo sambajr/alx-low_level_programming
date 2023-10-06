9ca45739365cd684c8a6e3b69bc8855210e860bb
sambajr <metasamba.ng007@gmail.com>
2023-10-06 03:02:06 -0700

    winner

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /* Include the standard input/output library*/
 /* Print the size of char*/
-int main(void)/*main function call*/
+int main(void)
+/*main function call*/
 {
 	fprintf(stderr, "Size of a char: %zu byte(s)\n", sizeof(char));
 	fprintf(stderr, "Size of an int : %zu byte(s)\n", sizeof(int));
-	fprintf(stderr, "Size of a long int: %zu byte(s)\n", sizeof(float));
+	fprintf(stderr, "Size of a long int: %zu byte(s)\n", sizeof(long));
 	fprintf(stderr, "Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	fprintf(stderr, "Size of a float: %zu byte(s)", sizeof(float));
+	fprintf(stderr, "Size of a float: %zu byte(s)\n", sizeof(float));
 /* Print the size of long long int*/
 	return (0);
 /* Return 0 to indicate successful execution End of the main functio*/
