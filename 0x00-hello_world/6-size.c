34c0cf5cb45d44a8099003915222411dc2c1846a
sambajr <metasamba.ng007@gmail.com>
2023-10-06 03:14:16 -0700

    betty styling

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,10 @@ int main(void)
 	fprintf(stderr, "Size of a char: %zu byte(s)\n", sizeof(char));
 	fprintf(stderr, "Size of an int : %zu byte(s)\n", sizeof(int));
 	fprintf(stderr, "Size of a long int: %zu byte(s)\n", sizeof(long));
-	fprintf(stderr, "Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	fprintf(stderr, "Size of a float: %zu byte(s)\n", sizeof(float));
+	fprintf(stderr, "Size of a long long int: %zu byte(s)\n", sizeof(double));
+	fprintf(stderr, "Size of a float: %zu byte(s)", sizeof(float));
 /* Print the size of long long int*/
 	return (0);
 /* Return 0 to indicate successful execution End of the main functio*/
 }
+/* main function returns standard error to the output and prints the sizee of values */
