294679be0807fd77ea250c4f1fb9c747366d327d
sambajr <metasamba.ng007@gmail.com>
2023-10-07 15:39:38 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,17 @@
  * Description:
  * The `main` function is the starting point of the program. It performs the
  * following tasks:
+ * Return:
+ * The `main` function returns an integer value of 0 to indicate successful
  */
 int main(void)
 {
 	/*we use the write function to print a message to the standard error*/
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	/*The main function returns 1 as requested*/
+	/*
+	*  The main function returns 1 as requeste
+	*
+	*/
 	return (1);
 }
 
