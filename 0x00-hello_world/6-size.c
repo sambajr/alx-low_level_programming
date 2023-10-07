16d728cc2e925aa3e4386cfd3ac752d362c37cd9
sambajr <metasamba.ng007@gmail.com>
2023-10-07 15:01:46 -0700

    mad

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(double));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
 	fprintf(stderr, "[Anything]");
 
 	return (0);
