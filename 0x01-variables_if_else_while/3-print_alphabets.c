7b646398b0a1b3a55a86a4ef9e92b6293ab39856
sambajr <metasamba.ng007@gmail.com>
2023-10-07 09:15:23 -0700

    main'

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* This program prints the alphabet in lowercase using the putchar function.
+* The putchar function is used only twice in this code.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		putchar(letter);
+	for (letter =  'A'; letter <= 'Z'; letter++)
+
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
