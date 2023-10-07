c6b18b654ede23ac4e0e80798d25f934e1ae73c4
sambajr <metasamba.ng007@gmail.com>
2023-10-07 10:32:02 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * The `main` function is the starting point of the program. It performs the
+ * following tasks:
+ * 1. Displays the numbers from 0 to 10, each on a separate line.
+ * 2. Uses the `printf` function to print these numbers.
+ *
+ * Return:
+ * The `main` function returns an integer value of 0 to indicate successful
+ * execution of the program.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		putchar(num + '0');
+	putchar('\n');
+	return (0);
+}
