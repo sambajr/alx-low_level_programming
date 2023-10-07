a91b506763de11ae74d39caa5ad09e62159efe92
sambajr <metasamba.ng007@gmail.com>
2023-10-07 07:02:13 -0700

    come

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point of the program
+*
+*Description: This program generates a random number,
+*              and prints a message based on the last digit's value.
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	int last;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	last = n % 10;
+
+	if (last > 5)
+	{
+		printf("last digit of %d is %d and greater than 5\n", n, last);
+	}
+	else if (last == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, last);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	}
+	return (0);
+}
+
+
+
+
+
+
+
