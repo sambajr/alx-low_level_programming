805e776f89b9f4ae20ed96f6cabbe82cbb94a35a
sambajr <metasamba.ng007@gmail.com>
2023-10-07 10:44:09 -0700

    reverse flash

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program prints the alphabet in lowercase using the putchar function.
+ * The putchar function is used only twice in this code.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	/* Use putchar to print each letter */
+		putchar(letter);
+	/* Use putchar to print a newline character */
+
+	putchar('\n');
+
+	return (0);
+}
