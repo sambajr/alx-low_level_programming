989f9488a82fb91125a969e2d64426d171f412e5
sambajr <metasamba.ng007@gmail.com>
2023-10-10 14:32:31 -0700

    new vs code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the main function of the program, and it serves
+ * as the entry point from where the program execution starts. It prints
+ * the "_putchar" string followed by a new line and returns 0.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n ");
+	return (0);
+}
