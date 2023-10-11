4202dfc7662aff8cd0a73cbeeb65f636f83ae309
sambajr <metasamba.ng007@gmail.com>
2023-10-11 00:54:16 -0700

    put'

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+void _putchar(char c)
+{
+    write(1, &c, 1); /* Write a single character */
+}
+
+
+
---
+++ 0x02-functions_nested_loops/print_alpha.c
@@ -0,0 +1,12 @@
+#include "_putchar.h"
+void print_alphabet(void)
+  {
+      char letter = 'a';
+  
+      while (letter <= 'z')
+      {
+          _putchar(letter);
+         letter++;
+      } 
+      _putchar('\n');
+ }
