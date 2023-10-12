ef3a4373a7168a1ef52677a5b36fc3fec49dfd2f
sambajr <metasamba.ng007@gmail.com>
2023-10-12 15:38:50 -0700

    man

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9 (excluding 2 and 4),
+ * followed by a new line.
+ */
+void print_most_numbers(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		if (digit != '2' && digit != '4')
+		{
+			_putchar(digit);
+		}
+	}
+
+	_putchar('\n');
+}
+
