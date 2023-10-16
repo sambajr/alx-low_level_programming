aaad7562adbf24f26e1be521c64119e3cfe9113e
sambajr <metasamba.ng007@gmail.com>
2023-10-16 14:42:33 +0100

    beach

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include "main.h"
+/**
+ *_strlen - Calculate the length of a string.
+ *@s: A pointer to a null-terminated string.
+ *
+ *Return: The length of the string (number of characters).
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
