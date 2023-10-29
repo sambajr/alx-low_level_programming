4abf3d33060fd7b97016526a5fc78fd0e83a5f59
sambajr <metasamba.ng007@gmail.com>
2023-10-29 21:40:00 +0100

    moon shot

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of cli agurment passed
+ * @argc: size of aragument
+ * @argv: pointer to the array of strings
+ * Return: Always (0) for {success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
