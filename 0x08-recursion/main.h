e47f61aa047686298a0bc3b4d96512903f7bbc95
sambajr <metasamba.ng007@gmail.com>
2023-10-29 12:08:17 +0100

    duke

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,4 +5,5 @@ int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
 void _puts_recursion(char *s);
 void _putchar(char c);
-
+int is_prime_helper(int n, int i);
+int is_prime_number(int n);
