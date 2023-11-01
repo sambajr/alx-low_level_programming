9a2c7d83dac7e94a07eee00374fb2c8b47638311
sambajr <metasamba.ng007@gmail.com>
2023-11-01 07:39:20 +0100

    malloc intro

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_array - creates an array of size[n] and initializing c
+ * @size: size of array
+ * @c: character in question
+ * Return: if sze is equal to 0 Return NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (size > 0)
+	{
+		ptr = (char *)malloc(size * sizeof(c));
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
+		return (ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
