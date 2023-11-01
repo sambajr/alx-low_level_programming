ebb3b0cea2a0fde6ac3513dacdb3aab3db416dbe
sambajr <metasamba.ng007@gmail.com>
2023-11-01 14:09:49 -0700

    CONCAT

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: pointer to a new string, or NULL if it fails
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k = 0, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		len += strlen(av[i]);
+	str = malloc(sizeof(char) * (len + ac + 1));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			str[k] = av[i][j];
+		str[k++] = '\n';
+	}
+	str[k] = '\0';
+	return (str);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,4 @@
+char *argstostr(int ac, char **av);
 char **strtow(char *str);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
