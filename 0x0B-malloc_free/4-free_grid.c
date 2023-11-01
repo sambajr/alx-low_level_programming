00711b3c8c2d285cac783d6501b6f7e1d6f63086
sambajr <metasamba.ng007@gmail.com>
2023-11-01 21:06:39 +0100

    Free memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ *  * free_grid - Frees a 2-dimensional grid previously created by alloc_grid.
+ *   * @grid: The pointer to the 2D array to be freed.
+ *    * @height: The height of the grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < height; i++)
+	{
+		if (grid[i] != NULL)
+		{
+			free(grid[i]);
+		}
+	}
+	free(grid);
+}
