9235a2d3d429cadb757dabf8f08269b01484b913
sambajr <metasamba.ng007@gmail.com>
2023-11-06 21:16:38 +0100

    PRINT me A dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,27 +16,13 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name:(nil)\n");
+		d->name = "(nil)";
 	}
-	else
-	{
-		printf("Name:%s\n", d->name);
-	}
-	if (d->age == 0.0)
-	{
-		printf("Age:(nil)\n");
-	}
-	else
-	{
-		printf("Age:%.6f\n", d->age);
-	}
+
 	if (d->owner == NULL)
 	{
-		printf("Owner:(nil)\n");
+		d->owner = "(nil)";
 	}
-	else
-	{
-		printf("Owner:%s\n", d->owner);
-	}
+	printf("Name:%s\nAge:%f\nOwner:%s\n", d->name, d->age, d->owner);
 
 }
