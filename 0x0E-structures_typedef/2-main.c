8ed2a0db60e86cdd9dc7093625a85707dde05a0a
sambajr <metasamba.ng007@gmail.com>
2023-11-06 19:35:19 +0100

    print_dog

---
+++ 0x0E-structures_typedef/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    struct dog my_dog;
+
+	        my_dog.name = " ";
+		my_dog.age = 3.3;
+		my_dog.owner = "Bob";
+			    print_dog(&my_dog);
+			        return (0);
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *  * print_dog - Prints a struct dog.
+ *   * @d: A pointer to a struct dog.
+ *    *
+ *     * Description: This function prints the attributes of a dog structure.
+ *      * If any attribute is NULL, it prints "(nil)" for that attribute.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+
+	}
+	if (d->name == NULL)
+	{
+		printf("Name:(nil\n)");
+	}
+	else
+	{
+		printf("Name:%s\n", d->name);
+	}
+	if (d->age == '\0')
+	{
+		printf("Age:(nil\n)");
+	}
+	else
+	{
+		printf("Age:%.1f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner:(nil\n)");
+	}
+	else
+	{
+		printf("Owner:%s\n", d->owner);
+	}
+
+}
