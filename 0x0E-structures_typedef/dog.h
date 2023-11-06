8f01e6d79c18329ec640a858029b366957a0099e
sambajr <metasamba.ng007@gmail.com>
2023-11-06 16:18:20 +0100

    BETTY wahala

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,13 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
