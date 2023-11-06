4c7ac96b45dbed7585a8fb5dc15ca12f0511582c
sambajr <metasamba.ng007@gmail.com>
2023-11-06 21:06:43 +0100

    scobby doo

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	    struct dog my_dog;
 
-	        my_dog.name = " ";
-		my_dog.age = 3.3;
+	        my_dog.name = "Poppy";
+		my_dog.age = 3.5;
 		my_dog.owner = "Bob";
 			    print_dog(&my_dog);
 			        return (0);
