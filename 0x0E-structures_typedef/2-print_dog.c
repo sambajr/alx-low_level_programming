426de9cf68aa4e709a2d41bb6238106f6d5a76f9
sambajr <metasamba.ng007@gmail.com>
2023-11-06 20:49:46 +0100

    print_your dogs here

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,15 +16,15 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name:(nil\n)");
+		printf("Name:(nil)\n");
 	}
 	else
 	{
 		printf("Name:%s\n", d->name);
 	}
-	if (d->age == '\0')
+	if (d->age == 0.0)
 	{
-		printf("Age:(nil\n)");
+		printf("Age:(nil)\n");
 	}
 	else
 	{
@@ -32,7 +32,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->owner == NULL)
 	{
-		printf("Owner:(nil\n)");
+		printf("Owner:(nil)\n");
 	}
 	else
 	{
