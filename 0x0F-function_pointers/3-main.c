d718e2da1c9c3bc19c98f6ab7e30d116dfbe5783
sambajr <metasamba.ng007@gmail.com>
2023-11-09 20:17:21 +0100

    TASK

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *  * main - performs simple operations
+ *   * @argc: number of arguments passed
+ *    * @argv: array of pointers to arguments
+ *     * Return: 0 on success, or error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", op_func(num1, num2));
+	return (0);
+}
