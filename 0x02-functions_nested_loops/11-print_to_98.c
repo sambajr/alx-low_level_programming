deec6282c09275d0b0121c49e34a71e0f51456b2
sambajr <metasamba.ng007@gmail.com>
2023-11-15 06:52:04 +0100

    print_t0-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,50 +1,32 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
-* print_to_98 - Prints natural numbers from n to 98.
-* @n: The starting number.
-*/
+ *  * print_to_98 - Prints all natural numbers from n to 98.
+ *   * @n: The starting number
+ */
 void print_to_98(int n)
 {
-	int i;
-	int num = 98
-
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; n++)
+		for (int i = n; i <= 98; i++)
 		{
-			if (i < 98)
-			{
-				_putchar('num' + '0');
-				_putchar('n' + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				_putchar('\n');:wq
-			}
+			printf("%d", i);
+			if (i != 98)
+		{
+			printf(", ");
+		}
 		}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (int i = n; i >= 98; i--)
 		{
-			if (i > 98)
+			printf("%d", i);
+			if (i != 98)
 			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				_putchar('\n');
+				printf(", ");
 			}
 		}
 	}
+	printf("\n");
 }
