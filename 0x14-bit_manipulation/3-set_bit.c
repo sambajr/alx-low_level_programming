5b8d07e4ccb5361a784f11dc8d2164e09e6da5e5
sambajr <metasamba.ng007@gmail.com>
2023-11-24 22:59:45 +0100

    LL_I_KNOW

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  * set_bit - sets the value of a bit to 1 at a given index
+ *   * @n: pointer to the number to be modified
+ *    * @index: the index, starting from 0 of the bit to set
+ *     *
+ *      * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(*n) * 8))
+		return (-1);
+	*n |= (1UL << index);
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,4 @@
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
