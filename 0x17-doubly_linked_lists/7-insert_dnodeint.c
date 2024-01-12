a22d850e3e553d2f661a4388404fd313051bc658
sambajr <metasamba.ng007@gmail.com>
2024-01-12 22:46:19 +0100

    A function that inserts a new node at a given position.

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the head of the list
+ * @idx: index of the list where the new node should be added, starts at 0
+ * @n: data for the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i = 0;
+
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (new_node);
+	}
+
+	while (i < idx - 1)
+	{
+		if (temp == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		temp = temp->next;
+		i++;
+	}
+
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next != NULL)
+	{
+		temp->next->prev = new_node;
+	}
+	temp->next = new_node;
+
+	return (new_node);
+}
